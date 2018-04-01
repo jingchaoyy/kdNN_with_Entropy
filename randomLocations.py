791eaf6d04f778afd9ba8331c9b693e44c96193f
jingchaoyy <jcyangt@gmail.com>
2018-04-01 18:10:45 -0400

    generating random points

--- randomLocations.py
+++ randomLocations.py
@@ -1,18 +1,45 @@
+""" Generating random points within a circular area
+Author: Jingchao Yang
+Date: Apr.1 2018
+"""
+
 import random
 import math
 
 # radius of the circle
-circle_r = 10
+circle_r = 100
 # center of the circle (x, y)
 circle_x = 5
 circle_y = 7
 
-# random angle
-alpha = 2 * math.pi * random.random()
-# random radius
-r = circle_r * random.random()
-# calculating coordinates
-x = r * math.cos(alpha) + circle_x
-y = r * math.sin(alpha) + circle_y
+def genPoints(numPs):
+    points = []
+    for i in range (numPs):
+        point = []
+        # random angle
+        alpha = 2 * math.pi * random.random()
+        # random radius
+        r = circle_r * random.random()
+        # calculating coordinates
+        x = r * math.cos(alpha) + circle_x
+        y = r * math.sin(alpha) + circle_y
+        point.append(x)
+        point.append(y)
+        points.append(point)
 
-print("Random point", (x, y))
+
+    return points
+
+
+print("Random points", genPoints(10))
+
+
+""" generate random world point geometries
+
+def newpoint():
+   return uniform(-180,180), uniform(-90, 90)
+
+points = (newpoint() for x in xrange(10))
+for point in points:
+   print point
+"""
