9ff91bd225a459d9699586106a35a94fe5f017f2
jingchaoyy <jcyangt@gmail.com>
2018-04-02 15:23:02 -0400

    assign neighbors with color types

--- knn.py
+++ knn.py
@@ -6,12 +6,38 @@ from sklearn.neighbors import NearestNeighbors
 import numpy as np
 import randomLocations
 
-# generating 100 points randomly
-points = randomLocations.genPoints(1000)
-X = np.array(points)
+# simple knn
 
-# X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-nbrs = NearestNeighbors(n_neighbors=len(X), algorithm='ball_tree').fit(X)
-# return distances and ranked neighbors
-distances, indices = nbrs.kneighbors(X)
-print(indices)
+def knn(p):
+    X = np.array(p)
+    nbrs = NearestNeighbors(n_neighbors=len(X), algorithm='ball_tree').fit(X)
+    # return distances and ranked neighbors (presented as point location in array points
+    distances, neighbors = nbrs.kneighbors(X)
+    return distances, neighbors
+
+""" Function defined to output all neighbors with 
+assigned point colors for input point latlon
+"""
+def typeRanks(pLatLon, neighbors):
+    if pLatLon in points:
+        pLocation = points.index(pLatLon)
+        pNeighbors = neighbors[pLocation]
+        neighborTypes = []
+        for n in pNeighbors:
+            neighborTypes.append(pTypes[n])
+        return neighborTypes
+    else:
+        print("Point not found")
+
+if __name__ == "__main__":
+    # generating 100 points randomly
+    points = randomLocations.genPoints(5)
+    print(points)
+    pTypes = randomLocations.pColor(points)
+    print(pTypes)
+
+    # calculating knn for each point
+    neighbors = knn(points)[1]
+    print(neighbors)
+    # e.g. find all neighbors with types for the first point
+    print(typeRanks(points[0],neighbors))
