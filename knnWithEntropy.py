2c105cfce93164e0115c26d47991960b601f8543
jingchaoyy <jcyangt@gmail.com>
2018-04-04 19:04:37 -0400

    updated

--- knnWithEntropy.py
+++ knnWithEntropy.py
@@ -2,15 +2,12 @@
 Author: Jingchao Yang
 Date: Apr.2 2018
 """
-
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
 import randomLocations
 import entropy
 
-
 # simple knn
-
 def knn(pS, fTs, pLatLon, k):
     X = np.array(pS)
     nbrsO = NearestNeighbors(n_neighbors=len(X), algorithm='ball_tree').fit(X)
@@ -28,17 +25,22 @@ def knn(pS, fTs, pLatLon, k):
             nbrs = NearestNeighbors(n_neighbors=i, algorithm='ball_tree').fit(X)
             # return distances and ranked neighbors (presented as point location in array points)
             distances, neighbors = nbrs.kneighbors(X)
+            # assign neighbors with their food type
             nbrTys = assignColor(pLatLon, pS, fTs, neighbors)
-            nbrTys = neighborsAfter + nbrTys[len(nbrTys)-1:len(nbrTys)]
-            print('\nOriginal',nbrTys)
-            diversity = entropy.calcShannonEnt(nbrTys)
+            # adding the latest neighbor to the adjusted neighbor list
+            nbrLast = neighborsAfter + nbrTys[len(nbrTys)-1:len(nbrTys)]
+            print('\nOriginal',nbrLast)
+            diversity = entropy.calcShannonEnt(nbrLast)
             # print(diversity)
             diversities.append(diversity)
-            kNNs.append(nbrTys)
+            kNNs.append(nbrLast)
+            # when more than 2 neighbors found, check if a switch of the last two can improve the diversity,
+            # and return the adjusted neighbor list
             if len(kNNs) > 2:
-                neighborsAfter = checkNeighbor(nbrTys)
-            else:
-                neighborsAfter = nbrTys
+                neighborsAfter = checkNeighbor(nbrLast)
+                print('Adjusted', neighborsAfter)
+            else:  # when less than 2 neighbors found, add to the neighbor list directly
+                neighborsAfter = nbrLast
 
     return neighborsAfter
 
@@ -74,7 +76,6 @@ def checkNeighbor(knns):
         knns = knns[:len(knns) - 2] + knns[len(knns) - 1:len(knns)] + knns[len(knns) - 2:len(knns) - 1]
     else:
         knns
-    print(knns)
     return knns
 
 
