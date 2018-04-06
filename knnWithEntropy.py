3dcff84410a9af2aeca33106a555ffe6eb45b217
jingchaoyy <jcyangt@gmail.com>
2018-04-06 13:32:22 -0400

    first step final
    first time accomplish the knn with enabled entropy for diversity
    calculation, find balance between distance and high diversity

--- knnWithEntropy.py
+++ knnWithEntropy.py
@@ -10,76 +10,67 @@ import time
 
 tStart = time.time()
 
-# simple knn
+""" Entropy enabled knn
+Algorithm will compute the diversity/ entropy each time when a new neighbor added
+and adjust the the sequence for the last two neighbor based on the entropy value 
+The final result is a balance between high diversity and distance
+"""
 def knn(pS, fTs, pLatLon, k):
     X = np.array(pS)
-    nbrsO = NearestNeighbors(n_neighbors=len(X), algorithm='ball_tree').fit(X)
-    # return distances and ranked neighbors (presented as point location in array points)
-    distancesO, neighborsO = nbrsO.kneighbors(X)
-    nbrTysO = assignColor(pLatLon, pS, fTs, neighborsO)
-    print("Origenal Neighbor Ranking: ", nbrTysO)
 
-    diversities = []  # storing diversity each time adding a new neighbor
-    kNNs = []
-    neighborsAfter = []
+    neighborsAfter = []  # Storing neighbors after each time getting neighbor check and switched
     # looking for knn one by one
     for i in range(len(pS) + 1):
         if i > 0:  # at least one nearest neighbor (i.e. itself)
             nbrs = NearestNeighbors(n_neighbors=i, algorithm='ball_tree').fit(X)
             # return distances and ranked neighbors (presented as point location in array points)
             distances, neighbors = nbrs.kneighbors(X)
-            # assign neighbors with their food type
-            nbrTys = assignColor(pLatLon, pS, fTs, neighbors)
+            # retrieving neighbors for target point
+            targetPNbrs = neighbors[pS.index(pLatLon)]
             # adding the latest neighbor to the adjusted neighbor list
-            nbrLast = neighborsAfter + nbrTys[len(nbrTys)-1:len(nbrTys)]
-            print('\nOriginal',nbrLast)
-            diversity = entropy.calcShannonEnt(nbrLast)
-            # print(diversity)
-            diversities.append(diversity)
-            kNNs.append(nbrLast)
+            neighborsAfter.append(targetPNbrs[len(targetPNbrs)-1:len(targetPNbrs)][0])
+            print('\nOriginal', assignFT(fTs,neighborsAfter))
             # when more than 2 neighbors found, check if a switch of the last two can improve the diversity,
             # and return the adjusted neighbor list
-            if len(kNNs) > 2:
-                neighborsAfter = checkNeighbor(nbrLast)
-                print('Adjusted', neighborsAfter)
+            if len(neighborsAfter) > 2:
+                neighborsAfter = checkNeighbor(fTs,neighborsAfter)
+                print('Adjusted', assignFT(fTs,neighborsAfter))
             else:  # when less than 2 neighbors found, add to the neighbor list directly
-                neighborsAfter = nbrLast
-
+                neighborsAfter = neighborsAfter
+    print('\n\n######################## Original Vs. Final Results #################################')
+    print('Original Neighbors:' ,targetPNbrs)
+    print('Original Food Type Rank:', assignFT(fTs,targetPNbrs))
+    print('Final Neighbors:', neighborsAfter)
+    print('Final Food Type Rank:', assignFT(fTs, neighborsAfter))
     return neighborsAfter
 
 
 """ Function defined to output all neighbors with 
 assigned point colors for input point latlon
 """
+def assignFT(fTs, nbors):
+    neighborTypes = []
+    # assign each neighbor with their color type
+    for n in nbors:
+        neighborTypes.append(fTs[n])
+    return neighborTypes
 
-
-def assignColor(pLatLon, pS, fT, nbors):
-    # check if point exits
-    if pLatLon in pS:
-        # find the point location
-        pLocation = pS.index(pLatLon)
-        # getting all the neighbors
-        pNeighbors = nbors[pLocation]
-        neighborTypes = []
-        # assign each neighbor with their color type
-        for n in pNeighbors:
-            neighborTypes.append(fT[n])
-        return neighborTypes
-    else:
-        print("Point not found")
-
-
-def checkNeighbor(knns):
+""" Function defined to check if a switch required between tha last two neighbor
+based on the entropy calculated
+"""
+def checkNeighbor(fTs, nbors):
+    # assign food type to all the neighbors first
+    knnT = assignFT(fTs, nbors)
     # calculating the diversity without the newly added neighbor
-    diversity1 = entropy.calcShannonEnt(knns[:len(knns) - 1])
+    diversity1 = entropy.calcShannonEnt(knnT[:len(knnT) - 1])
     # calculating the diversity if replace the last added neighbor with the newly added neighbor
-    diversity2 = entropy.calcShannonEnt(knns[:len(knns) - 2] + knns[len(knns) - 1:len(knns)])
-    # switching the last two neighbors since this can give a higher entropy value for k-1
+    diversity2 = entropy.calcShannonEnt(knnT[:len(knnT) - 2] + knnT[len(knnT) - 1:len(knnT)])
+    # switching the last two neighbors if adding the later added neighbor first can give a higher entropy
     if diversity2 > diversity1:
-        knns = knns[:len(knns) - 2] + knns[len(knns) - 1:len(knns)] + knns[len(knns) - 2:len(knns) - 1]
+        nbors = nbors[:len(nbors) - 2] + nbors[len(nbors) - 1:len(nbors)] + nbors[len(nbors) - 2:len(nbors) - 1]
     else:
-        knns
-    return knns
+        nbors
+    return nbors
 
 
 if __name__ == "__main__":
