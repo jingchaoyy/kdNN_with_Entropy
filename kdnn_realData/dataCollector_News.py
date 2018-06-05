1cff97f9c79af82ebacd2b250db431ece949a9b7
jingchaoyy <jcyangt@gmail.com>
2018-06-05 16:25:40 -0400

    initialize data collector

---
+++ kdnn_realData/dataCollector_News.py
@@ -0,0 +1,21 @@
+"""
+Created on 6/5/18
+
+@author: Jingchao Yang
+"""
+import pandas as pd
+
+allNews = pd.read_csv('yelpData/news_Separate.csv')
+
+categories = allNews['CATEGORY']
+lat, lon = allNews['LAT'], allNews['LON']
+
+cates, coors = [],[]
+for i in range(len(categories)):
+    cate = []
+    for j in categories[i].split(','):
+        cate.append(j)
+    cates.append(cate)
+    coors.append((lat[i],lon[i]))
+
+# print(cates)
