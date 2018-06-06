764768c8349f4839565821fdc32afae49f459d80
jingchaoyy <jcyangt@gmail.com>
2018-06-06 18:41:07 -0400

    update variable names

--- dataGenerator.py
+++ dataGenerator.py
@@ -11,10 +11,10 @@ def randomData(num=100, radius=500, query_x=0, query_y=0, catNum=5, catRange=26)
     # num: Num of Entry
     # radius: Range of query
     # query_lat, query_lng: Location of query point
-    # catNum: Number of categories for each entry
+    # catNum: max Number of categories for each entry
     # catRange: Range of categories
-    catList = []
-    locList = []
+    allCategories = []
+    allPoints = []
     # catSum = list(string.ascii_uppercase)
     for i in range(num):
         catTmp = set()
@@ -22,13 +22,13 @@ def randomData(num=100, radius=500, query_x=0, query_y=0, catNum=5, catRange=26)
         r = radius * random.random()
         loc_x = r * math.cos(alpha) + query_x
         loc_y = r * math.sin(alpha) + query_y
-        locList.append((loc_x, loc_y))
+        allPoints.append((loc_x, loc_y))
         while len(catTmp) < catNum:
             catTmp.add(random.randint(0, catRange))
-        catList.append(list(catTmp))
-    print(locList)
-    print(catList)
-    return locList, catList
+            allCategories.append(list(catTmp))
+    # print(allPoints)
+    # print(allCategories)
+    return allPoints, allCategories
 
 
 def main():
