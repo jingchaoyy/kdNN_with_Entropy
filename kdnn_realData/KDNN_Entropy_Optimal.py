14a5df94fafde52ada33d88eba0424ba4915c124
jingchaoyy <jcyangt@gmail.com>
2018-06-07 23:17:14 -0400

    adding predefined weight during entropy calculating

--- kdnn_realData/KDNN_Entropy_Optimal.py
+++ kdnn_realData/KDNN_Entropy_Optimal.py
@@ -16,22 +16,27 @@ In a way, it provides all non-dominated sets
 """
 
 
-def knn(pS, fTs, pLatLon, k):
-    newpS = pLatLon + pS  # adding user location to the list
-    X = np.array(newpS)
+def knn(pS, fTs, pid, k, wFTs):
+    # pS: all latlons
+    # fTs: all fTs associated with pS
+    # pid: user location, represented by restaurant ID
+    # k: number of nbors
+    # wFTs: set combine fTs with weight
+
+    X = np.array(pS)
     nonDominated = []
     targetPNbrs = ''
     for i in range(len(pS) + 1):
-        if i > 1:  # at least one nearest neighbor (i.e. itself)
+        if i > 0:  # at least one nearest neighbor (i.e. itself)
             nbrs = NearestNeighbors(n_neighbors=i, algorithm='ball_tree').fit(X)
             # return distances and ranked neighbors (presented as point location in array points)
             distances, neighbors = nbrs.kneighbors(X)
             # retrieving neighbors for target point
-            targetPNbrs = neighbors[0]
-            targetDistances = distances[0]
+            targetPNbrs = neighbors[pid]
+            targetDistances = distances[pid]
 
-            tnList = list(targetPNbrs)[1:]  # starting from the second one, first one is the user itself
-            tdList = list(targetDistances)[1:]
+            tnList = list(targetPNbrs)
+            tdList = list(targetDistances)
 
             print('\nOriginal NID', tnList)
             print('Original', assignFT(fTs, tnList))
@@ -43,7 +48,7 @@ def knn(pS, fTs, pLatLon, k):
                 # neighborsAfter = checkNeighbor(fTs, subs)
 
                 runTStart = time.time()
-                resultNbor = checkNeighbor(fTs, subs)
+                resultNbor = checkNeighbor(fTs, subs, wFTs)
                 runTEnd = time.time()
                 neighborsAfter = resultNbor[0]  # set of neighbors
                 divAfter = resultNbor[1]  # entropy of the neighbor set
@@ -63,15 +68,16 @@ def knn(pS, fTs, pLatLon, k):
                 if len(tnList) == k:
                     nonDominated.append((neighborsAfter, maxDist, divAfter, runT))
 
-                elif nonDominated[-1][0] != neighborsAfter:  # see if the last added nonDominated sets is tha same
+                elif set(nonDominated[-1][0]) != set(
+                        neighborsAfter):  # see if the last added nonDominated sets is tha same
                     # as the latest one, if the same, ignore the latest one
                     nonDominated.append((neighborsAfter, maxDist, divAfter, runT))
 
-            # else:  # when less than minimum required neighbors found, add to the neighbor list directly
-            #     print('nondominated neighbor set:', tnList)
-            #     if len(tnList) == k:  # add the first find knn set to the nonDominated list
-            #         maxDisttd = max(tdList)
-            #         nonDominated.append((tnList, maxDisttd))
+                    # else:  # when less than minimum required neighbors found, add to the neighbor list directly
+                    #     print('nondominated neighbor set:', tnList)
+                    #     if len(tnList) == k:  # add the first find knn set to the nonDominated list
+                    #         maxDisttd = max(tdList)
+                    #         nonDominated.append((tnList, maxDisttd))
 
     print('\n\n######################## Original Vs. Final Results #################################')
     print('Original Neighbors:', tnList)
@@ -99,7 +105,7 @@ def assignFT(fTs, nbors):
     # assign each neighbor with their color type
     for n in nbors:
         neighborTypes.append(
-            fTs[n - 1])  # n - 1 due to the sequence all push back by 1 after adding user location to the locationlist
+            fTs[n])
     return neighborTypes
 
 
@@ -108,7 +114,7 @@ based on the entropy calculated
 """
 
 
-def checkNeighbor(fTs, subsets):
+def checkNeighbor(fTs, subsets, wFTs):
     subsetFTList, subsetList = [], []
     for ss in subsets:
         # assign food type to all the neighbors first
@@ -121,7 +127,7 @@ def checkNeighbor(fTs, subsets):
         for j in i:  # in each restaurant
             for k in j:  # getting each category
                 sets.append(k)
-        diversity = entropy.calcShannonEnt(sets)
+        diversity = entropy.calcShannonEnt(sets, wFTs)
         divList.append(diversity)
 
     # looking for the max entropy
