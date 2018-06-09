01b7f6e2fbadac1d34795f9afac85434450b2e55
jingchaoyy <jcyangt@gmail.com>
2018-06-09 12:49:59 -0400

    collect all nonDominated sets and calculate the average after looping

--- kdnn_realData/driver.py
+++ kdnn_realData/driver.py
@@ -5,9 +5,9 @@ Created on 6/7/18
 """
 ############## data source ##############
 # from kdnn_realData import dataCollector_Yelp
-# from kdnn_realData import dataCollector_News
+from kdnn_realData import dataCollector_News
 # from kdnn_realData import dataCollector_Pubmed
-import dataGenerator
+# import dataGenerator
 
 ############## algorithms ##############
 from kdnn_realData import KDNN_Entropy_Greedy
@@ -18,24 +18,25 @@ from kdnn_realData import KDNN_Union_Optimal
 
 import time
 import random
+import numpy as np
 
 if __name__ == "__main__":
     tStart = time.time()
 
     kk = 6  # Num of neighbors
-    datasetRange = 20
-
+    datasetRange = 1000
     loops = 100
+    resultPool = []
 
     # ############## Yelp #################
     # pSets = dataCollector_Yelp.allPoints
     # fTypes = dataCollector_Yelp.allCategories
     # userAddr = [(35.04728681, -80.99055881)]  # input user location
 
-    # ############## News #################
-    # pSets = dataCollector_News.allPoints
-    # fTypes = dataCollector_News.allCategories
-    # # userAddr = [(-12.97221841, -38.50141361)]  # input user location
+    ############## News #################
+    pSets = dataCollector_News.allPoints
+    fTypes = dataCollector_News.allCategories
+    # userAddr = [(-12.97221841, -38.50141361)]  # input user location
 
     # ############## Publication #################
     # pSets = dataCollector_Pubmed.allPoints
@@ -43,16 +44,17 @@ if __name__ == "__main__":
     # userAddr = [(52.15714851, 4.4852091)]  # input user location
 
     ############## Synthetic Data #################
-    x, y = 0, 0
-    userAddr = [(x, y)]  # input user location
-    numRecords = 100
-    searchRange = 500
-    cateNum = 10
-    cateRange = 200
-    pSets, fTypes = dataGenerator.randomData(numRecords, searchRange, x, y, cateNum, cateRange)
+    # x, y = 0, 0
+    # userAddr = [(x, y)]  # input user location
+    # numRecords = 100
+    # searchRange = 500
+    # cateNum = 10
+    # cateRange = 200
+    # pSets, fTypes = dataGenerator.randomData(numRecords, searchRange, x, y, cateNum, cateRange)
+
 
     for i in range(loops):
-        id = random.randint(0, datasetRange-1)
+        id = random.randint(0, datasetRange - 1)
         getLoc = pSets[id]
         print('\n@@@@@@@@@@@@ User ID/Location', id, getLoc, '@@@@@@@@@@@@')
 
@@ -71,9 +73,17 @@ if __name__ == "__main__":
         # select an algorithm for kdnn
         neighbors = KDNN_Entropy_Greedy.knn(pSets[:datasetRange], fTypes, id,
                                             kk, ftWW)  # start from p0, collect all 6 nearest restaurant
-        print('\n\n######################## Non Dominated #################################')
-        for nd in neighbors:
-            print('(Nbor, Dist, Div, RT): ', nd)
+        # print('\n\n######################## Non Dominated #################################')
+        # for nd in neighbors:
+        #     print('(Nbor, Dist, Div, RT): ', nd)
+
+        resultPool.append(neighbors)
+
+    print('\n\n######################## Non Dominated #################################')
+    print('       Dist         Diversity       Runtime')
+    resultPool = np.array(resultPool)
+    resultPool = resultPool.sum(axis=0)  # sum by columns
+    print(resultPool / loops)  # all get average
 
 tEnd = time.time()
 print("\nTotal time: ", tEnd - tStart, "seconds")
