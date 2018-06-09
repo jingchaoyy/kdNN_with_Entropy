e15637b8a02a6e8f56e548fb525d86ed5b9b05eb
jingchaoyy <jcyangt@gmail.com>
2018-06-09 19:09:45 -0400

    Correlation Between Neighbor Size and Runtime (neighbor category size based)

---
+++ kdnn_realData/3_exp.py
@@ -0,0 +1,75 @@
+"""
+Created on 6/9/18
+
+@author: Jingchao Yang
+"""
+############## data source ##############
+import dataGenerator
+
+############## algorithms ##############
+from kdnn_realData import KDNN_Entropy_Greedy
+from kdnn_realData import KDNN_Entropy_Hybrid
+from kdnn_realData import KDNN_Entropy_Optimal
+from kdnn_realData import KDNN_Union_Greedy
+from kdnn_realData import KDNN_Union_Optimal
+
+import time
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    kk = 6  # Num of neighbors
+    datasetRange = 20
+    loops = 10
+
+    ############# Synthetic Data #################
+    x, y = 0, 0
+    numRecords = 100
+    searchRange = 500
+    cateNum = [3, 5, 10]  # 3, 5, 10
+    cateRange = 200
+    colors = ['r', 'g', 'b']
+
+    fig, ax = plt.subplots()
+
+    for i in range(len(cateNum)):
+        pSets, fTypes = dataGenerator.randomData(numRecords, searchRange, x, y, cateNum[i], cateRange)
+        resultPool = []
+        runtimes = []
+        for n in range(datasetRange):
+            if n >1:
+                tStart = time.time()
+                for j in range(loops):
+                    id = random.randint(0, n - 1)
+                    getLoc = pSets[id]
+                    print('\n@@@@@@@@@@@@ User ID/Location', id, getLoc, '@@@@@@@@@@@@')
+
+                    allFt, preWeight = [], []
+                    for ftSet in fTypes:  # get all fts (with duplicates)
+                        for ft in ftSet:
+                            allFt.append(ft)
+                    allFt = KDNN_Union_Greedy.Remove(allFt)  # ft without duplicate
+                    for j in range(len(allFt)):
+                        preWeight.append(random.uniform(0, 1))
+
+                    ftWW = []  # bound ft with weight
+                    for k in range(len(allFt)):
+                        ftWW.append((allFt[k], preWeight[k]))
+
+                    # select an algorithm for kdnn
+                    neighbors = KDNN_Entropy_Greedy.knn(pSets[:n], fTypes, id,
+                                                  kk, ftWW)  # start from p0, collect all 6 nearest restaurant
+
+
+                tEnd = time.time()
+                runtimes.append(tEnd - tStart)
+
+
+        ax.plot(range(2,datasetRange), runtimes, colors[i], label=cateNum[i])
+
+    legend = ax.legend(loc='upper left', shadow=True, fontsize='large')
+    plt.title("KDNN_Entropy_Greedy: Correlation Between Neighbor Size and Runtime")
+    plt.xlabel("Neighbor Size")
+    plt.ylabel("Runtime")
+    plt.show()
